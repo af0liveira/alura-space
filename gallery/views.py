090b0d25b7af19990b1190053250cc7c2eba1110
af0liveira <af0liveira@icloud.com>
2023-04-07 01:23:11 +0200

    POC: Automatic creation of image cards from a database.
    The code was modified to create the photo cards on the main page
    automatically from a database.
    The database, at the moment, is represented by a dictionary with only
    name and descriptin of two images.
    
    The images are not updated yet, though.

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'gallery/index.html')
+
+    data = {
+        1: {
+            'name': 'Nebulosa de Carina',
+            'description': 'webbtelescope.org / NASA / James Webb',
+        },
+        2: {
+            'name': 'Galáxia NGC 1079',
+            'description': 'nasa.org / NASA / Hubble',
+        }
+    }
+
+    return render(request, 'gallery/index.html', dict(cards=data))
 
 def image(request):
     return render(request, 'gallery/image.html')
