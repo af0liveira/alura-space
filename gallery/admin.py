47896abc955609fa11ef353841e8e7424ac3aadf
af0liveira <af0liveira@icloud.com>
2023-04-07 23:43:26 +0200

    Configured admin options for the `gallery` app.
    The database (model Photo) can now be managed via Django Admin.

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from gallery.models import Photo
+
+class ListPhotos(admin.ModelAdmin):
+    """Customize the display of the Photo model in Django Admin."""
+
+    list_display = ('id', 'title', 'caption')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+admin.site.register(
+    Photo,
+    ListPhotos,
+    )
