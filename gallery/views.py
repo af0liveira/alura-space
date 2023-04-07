3759a6b1e056b1801dc619c42d2941fd904fda17
af0liveira <af0liveira@icloud.com>
2023-04-07 22:39:24 +0200

    Implemented dynamic references for the images.
    Now, when clicking the thumbnail in the main page, the correct photo
    will be opened.

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from gallery.models import Photo
 
@@ -6,5 +6,6 @@ def index(request):
     photos = Photo.objects.all()
     return render(request, 'gallery/index.html', dict(cards=photos))
 
-def image(request):
-    return render(request, 'gallery/image.html')
+def image(request, photo_id):
+    photo = get_object_or_404(Photo, pk=photo_id)
+    return render(request, 'gallery/image.html', dict(photo=photo))
