bac8e97619eb6e5e068678966a9351993bfae504
af0liveira <af0liveira@icloud.com>
2023-04-07 02:55:57 +0200

    Implemented database for the photos.
    The data model was implemented as the class Photo in `gallery/models.py`
    
    Additionally, the the INSTALLED_APPS list was updated with a better
    reference for the `gallery` app.

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    """Class for representing an photograph in the database."""
+    title = models.CharField(max_length=100, null=False, blank=False)
+    caption = models.CharField(max_length=250, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    filename = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f'Photo [title={self.title}]'
