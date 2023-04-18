3a8a21d3a1501694c801e7023a4539d966f21755
af0liveira <af0liveira@icloud.com>
2023-04-18 22:41:19 +0200

    Implemented URL and views to enable login and signup pages
    The login and signup pages only display a header with their own names,
    but the logicas for calling them has been implemented.

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from users.views import login, signup
 
-urlpatterns = []
+urlpatterns = [
+    path('login', login, name='login'),
+    path('signup', signup, name='signup'),
+]
