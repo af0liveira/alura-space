20140cd760979b5db68a70a6b09d7645b3490f0a
af0liveira <af0liveira@icloud.com>
2023-04-20 18:34:33 +0200

    Implemented route for new user signup.
    New users can now sign up to the website.
    The signup process will ensure that duplicate user names cannot be
    created.
    User will also be asked to type their password twice in order to avoid
    typing mistakes at password creation.
    At last, after a successful signup, the user will be redirected to the
    login page.

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from users.forms import LoginForm, SignupForm
 
@@ -8,4 +9,29 @@ def login(request):
 
 def signup(request):
     form = SignupForm()
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+
+            if form['password'].value() != form['password_confirmation'].value():
+                return redirect('signup')
+
+            username = form['login_name'].value()
+            password = form['password'].value()
+            email = form['email'].value()
+
+            if User.objects.filter(username=username).exists():
+                return redirect('signup')
+
+            new_user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+            )
+
+            new_user.save()
+            return redirect('login')
+    
     return render(request, 'users/signup.html', dict(form=form))
