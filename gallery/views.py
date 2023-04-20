da44d57128388f00888bb74a7e8ab92818237ad0
af0liveira <af0liveira@icloud.com>
2023-04-20 23:27:56 +0200

    Content access restricted to authenticated users.
    In order to search content or see a full image, the user is now required
    to log in.

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
 from gallery.models import Photo
 
@@ -7,10 +8,18 @@ def index(request):
     return render(request, 'gallery/index.html', dict(cards=photos))
 
 def image(request, photo_id):
+    if not request.user.is_authenticated:
+        messages.error(request, "Faça seu login para prosseguir!")
+        return redirect('login')
+
     photo = get_object_or_404(Photo, pk=photo_id)
     return render(request, 'gallery/image.html', dict(photo=photo))
 
 def search(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Faça seu login para prosseguir!")
+        return redirect('login')
+
     photos = Photo.objects.order_by('-date_added').filter(public=True)
 
     if 'search' in request.GET:
