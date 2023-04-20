22ed9fdba9441d3eb5fa52767f2a5cd1458fa023
af0liveira <af0liveira@icloud.com>
2023-04-20 19:17:57 +0200

    Implemented login logic.
    Upon successful login authentication, the user is redirected to the the
    index page of the website.
    If the authentication fails, the user is redirected back to the login
    page.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 
 from users.forms import LoginForm, SignupForm
 
 def login(request):
     form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form['login_name'].value()
+            password = form['password'].value()
+
+            user = auth.authenticate(
+                request,
+                username=username,
+                password=password,
+            )
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect('index')
+            else:
+                return redirect('login')
+
     return render(request, 'users/login.html', dict(form=form))
 
 def signup(request):
